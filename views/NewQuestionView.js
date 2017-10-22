8fae6e58c5da282b4df51606c7d0c3f019f79386
Jesse Weigel <jesseweigel@gmail.com>
2017-10-21 20:28:43 -0400

    feat: add ability to add questions to decks

--- views/NewQuestionView.js
+++ views/NewQuestionView.js
@@ -1,13 +1,71 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableNativeFeedback, TextInput, KeyboardAvoidingView, AsyncStorage } from 'react-native'
+import { purple, white } from '../utils/colors'
 
 export default class NewQuestionView extends React.Component {
+
+
+    state = {
+      question: 'Enter Question',
+      answer: 'Enter Answer'
+    }
+
+    handleQuestionChange = (input) => {
+      this.setState(() => ({
+        question: input
+      }))
+    }
+
+    handleAnswerChange = (input) => {
+      this.setState(() => ({
+        answer: input
+      }))
+    }
+
+    handleSubmit = () => {
+      const { deck } = this.props.navigation.state.params
+
+      AsyncStorage.getItem('decks', (err, result) => {
+        const results = JSON.parse(result)
+        const theseQuestions = results[deck.title].questions
+        AsyncStorage.mergeItem('decks', JSON.stringify({
+          [deck.title]: {title: deck.title, questions: [{
+            question: this.state.question,
+            answer: this.state.answer
+          }, ...theseQuestions ]}
+        }), () => {
+          AsyncStorage.getItem('decks', (err, result) => {
+            console.log(JSON.parse(result))
+          })
+        })
+      })
+
+
+    }
+
   render () {
+    const { question, answer } = this.state
     return (
-      <View style={styles.container}>
-        <Text>New Question</Text>
-      </View>
-    )
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <Text style={styles.title}>What is the title of your new deck?</Text>
+        <TextInput
+          value={question}
+          style={styles.input}
+          onChangeText={this.handleQuestionChange}
+        />
+        <TextInput
+          value={answer}
+          style={styles.input}
+          onChangeText={this.handleAnswerChange}
+        />
+        <TouchableNativeFeedback
+          onPress={() => this.handleSubmit()} >
+          <View style={[styles.btn, styles.invertedBtn]}>
+            <Text>Add to Deck</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </KeyboardAvoidingView>
+          )
   }
 }
 
@@ -17,5 +75,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  title: {
+    fontSize: 24,
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 8,
+    margin: 50
+  },
+  btn: {
+    padding: 80,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 16,
+    borderRadius: 2,
+    borderWidth: 2,
+  },
+  invertedBtn: {
+    borderColor: purple,
   }
 })
